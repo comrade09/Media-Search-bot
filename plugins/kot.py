eff8a035052d435c3201183445cce1a08c404a91
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-08-25 22:33:26 +0530

    Update kot.py

--- plugins/kot.py
+++ plugins/kot.py
@@ -13,32 +13,6 @@ import random
 from pyrogram import Client, filters
 import time
 
-@Client.on_message(filters.command("quiz"))
-async def start_quiz(client, message):
-    chat_id = message.chat.id
-    await client.send_message(chat_id, "Quiz has started! I will send you a question every 30 seconds. Answer with the correct option.")
-
-    while True:
-        question = random.choice(questions)
-        await send_question(client, chat_id, question)
-        await asyncio.sleep(30)
-
-questions = [
-    {
-        "question": "What is the capital of France?",
-        "options": ["Paris", "London", "Berlin", "Madrid"],
-        "correct_option": 0,
-        "explanation": "The capital of France is Paris."
-    },
-    {
-        "question": "Which planet is known as the 'Red Planet'?",
-        "options": ["Venus", "Mars", "Jupiter", "Saturn"],
-        "correct_option": 1,
-        "explanation": "Mars is known as the 'Red Planet' due to its reddish appearance."
-    },
-    # Add more questions here
-]
-
 async def send_question(client, chat_id, question):
     options = question["options"]
     question_text = question["question"]
@@ -52,10 +26,20 @@ async def send_question(client, chat_id, question):
         question=question_text,
         options=poll_options,
         type="quiz",
-        correct_option_id=correct_option
+        correct_option_id=correct_option,
+        is_anonymous=False
     )
 
     if explanation:
-        await asyncio.sleep(30)  # Wait for 30 seconds
-        await client.send_message(chat_id, explanation)
+        await asyncio.sleep(2)  # Wait for 30 seconds
+        await client.send_message(chat_id, f"{explanation} \n this bot is made by @python_itachi ")
+
+@Client.on_message(filters.command("quiz"))
+async def start_quiz(client, message):
+    chat_id = message.chat.id
+    await client.send_message(chat_id, "Quiz has started! I will send you a question every 1 hour. Answer with the correct option.")
+
+    for question in questions:
+        await send_question(client, chat_id, question)
+        await asyncio.sleep(10)  # Wait for 1 hour between questions
 
